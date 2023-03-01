1c609ea0234a03ee3400d3c38bb71a86ecf288b1
JuloushVolley <julien.schoeffre@sfr.fr>
2023-03-01 16:09:46 +0100

    Create panier.php

---
+++ panier.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Page d'accueil</title>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" type="text/css" href="tpfinal.css" />
+  </head>
+  <body>
+    <p id="p1">
+      Bonjour madame, monsieur ,<br />
+      Sur mon site de streaming payant pour des films de la catégorie action ou drama.<br>
+      Je vous invite donc a vous renseigner sur les page dedier aux catergorie de films.<br>
+      Pour pouvoir acheter les film ils faudra que vous creier un compte sur le lien qui se nomme souscription.<br>
+      Par contre si vous vouler vous désinscrire il faudra que vous appyer sur le bouton desouscription, en vous munissant de votre e-mail et votre mot de passe pour vous désinscrire<br>
+    </p>
+    <form>
+      <button type="submit" id="bouton1"><a href="film.php" >film</button>
+        <br>
+      <button type="submit" id="bouton2"><a href="panier.php">panier</button>
+        <br>
+      <button type="submit" id="bouton3"><a href="souscription.php">souscription</button>
+        <br>
+      <button type="submit" id="bouton4"><a href="desouscription.php">desouscription</button>
+    </form>
+  </body>
+</html>
