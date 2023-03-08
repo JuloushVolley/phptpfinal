c75b5a95c2b368076803ff04e8555c896e1ef535
JuloushVolley <julien.schoeffre@sfr.fr>
2023-03-08 16:41:16 +0100

    Update film.php

--- film.php
+++ film.php
@@ -10,16 +10,194 @@
       Bonjour madame, monsieur ,<br />
       Sur cette page vous pourrez avoir acces aux film de deux catégorie dram et action<br>
       Chaque film aura un lien qui vous montrera une bande d'annonce et un autre qui vous montrera les autre film fait par le réalisateur.<br>
-      Chaque film coutera entre 4,55$ et 5,55$.<br>
+      Chaque film coutera 5,99$.<br>
     </p>
-    <form>
-      <button type="submit" id="bouton1"><a href="film.php" >film</button>
+    <p>
+    <table class='tab'>
+  <thead>
+    <tr>
+      <th>Numero du film</th>
+      <th>Nom</th>
+      <th>Déscription</th>
+      <th>réalisateur</th>
+      <th>Type</th>
+      <th>prix</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>
+      <a href="https://www.youtube.com/watch?v=pWK5crMuhHY" ><img src="john1.jpg" width="100%" height="100%" ></a></td>
+      <td>Ancien tueur à gages repenti, John Wick vient de perdre sa femme Helen, décédée des suites d'une longue maladie. Peu après l'enterrement, John reçoit un colis, contenant un chiot femelle beagle nommée Daisy et une lettre : il s'agit d'un cadeau posthume d'Helen pour l'aider à surmonter sa disparition. Il mène une vie sans histoire, jusqu’à ce qu’un malfrat sadique nommé Iosef Tarasof remarque sa voiture. John refuse de la lui vendre. Iosef n’acceptant pas qu’on lui résiste, s’introduit chez John avec deux complices pour voler la Mustang, et tuer sauvagement Daisy… John remonte la piste de Iosef jusqu’à New York. Un ancien contact, Aurelio, lui apprend que le malfrat est le fils unique d’un grand patron de la pègre, Viggo Tarasof. La rumeur se répand rapidement dans le milieu : le légendaire tueur cherche Iosef. Viggo met à prix la tête de John : quiconque l’abattra touchera une énorme récompense. John a désormais tous les assassins de New York à ses trousses.</td>
+      <td><a href="https://fr.wikipedia.org/wiki/Chad_Stahelski" >Chad Stahelski </td>
+      <td>Action</td>
+      <td>5,55$</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td><a href="https://www.youtube.com/watch?v=ChpLV9AMqm4" ><img src="john2.jpg" width="100%" height="100%" > </a></td>
+      <td>John Wick est de nouveau obligé de sortir de sa retraite anticipée par un ancien associé comptant prendre le contrôle d’une confrérie d’assassins internationaux. Ayant juré de l’aider, John se rend à Rome où il doit affronter certains des tueurs les plus dangereux au monde.</td>
+      <td><a href="https://fr.wikipedia.org/wiki/Chad_Stahelski" >Chad Stahelski </td>
+      <td>Action</td>
+      <td>5,55$</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td><a href="https://www.youtube.com/watch?v=flyjuEwJMhA" ><img src="john 3.jpg" width="100%" height="100%" ></a></td>
+      <td>John Wick a transgressé une règle fondamentale: il a tué à l'intérieur même de l'Hôtel Continental. Excommunié, tous les services liés au Continental lui sont fermés et sa tête est mise à prix. John se retrouve sans soutien, traqué par tous les plus dangereux tueurs du monde.</td>
+      <td><a href="https://fr.wikipedia.org/wiki/Chad_Stahelski" >Chad Stahelski </td>
+      <td>Action</td>
+      <td>5,55$</td>
+    </tr>
+    <tr>
+      <td>4</td>
+      <td><a href="https://www.youtube.com/watch?v=V-D6iFo2LeQ" ><img src="topgun.jpg" width="100%" height="100%" ></a></td>
+      <td>Top Gun est un film d'action américain réalisé par Tony Scott et sorti en 1986. Il met notamment en scène dans les rôles principaux Tom Cruise, Kelly McGillis, Anthony Edwards et Val Kilmer. L'action se déroule en partie à Topgun, une école de combat aérien de la marine américaine.</td>
+      <td><a href=https://fr.wikipedia.org/wiki/Tony_Scott" >Tony Scott </td>
+      <td>Action</td>
+      <td>5,55$</td>
+    </tr>
+    <tr>
+      <td>5</td>
+      <td><a href=https://www.youtube.com/watch?v=V4gQdk1nAn0" ><img src="mav.jpg" width="100%" height="100%" ></a></td>
+      <td>Après plus de 30 ans de service en tant que l'un des meilleurs aviateurs de la Marine, Pete "Maverick" Mitchell est à sa place, repoussant les limites en tant que pilote d'essai courageux et esquivant l'avancement de grade qui le mettrait à la terre. Entraînant de jeunes diplômés pour une mission spéciale, Maverick doit affronter les fantômes de son passé et ses peurs les plus profondes, aboutissant à une mission qui exige le sacrifice ultime de ceux qui choisissent de la piloter.</td>
+      <td><a href="https://fr.wikipedia.org/wiki/Joseph_Kosinski" >Joseph Kosinski</td>
+      <td>Action</td>
+      <td>5,55$</td>
+    </tr>
+    <tr>
+      <td>6</td>
+      <td><a href="https://www.youtube.com/watch?v=rl2bysiyltg" ><img src="mi3.jpg" width="100%" height="100%" ></a></td>
+      <td>Ethan espérait avoir tourné une page en quittant le service actif de la Force Mission Impossible pour un poste de formateur ; pouvoir enfin mener une vie normale, se consacrer tout entier à sa ravissante épouse, Julia. Cependant, lorsque Lindsey, la plus brillante recrue de l'IMF tombe à Berlin, Ethan se sent moralement obligé de lui porter secours.</td>
+      <td><a href="https://fr.wikipedia.org/wiki/J._J._Abrams" > J.J. Abrams </td>
+      <td>Action</td>
+      <td>5,55$</td>
+    </tr>
+    <tr>
+      <td>7</td>
+      <td><a href="https://www.youtube.com/watch?v=m31C9DofmSo" ><img src="mi4.jpg" width="100%" height="100%" ></a></td>
+      <td>Ethan Hunt et l'agence IMF sont désavoués par le gouvernement américain qui leur reproche une attaque terroriste au Kremlin. Laissé pour compte, Hunt doit laver l'honneur de l'agence et éviter une nouvelle attaque.</td>
+      <td><a href="https://fr.wikipedia.org/wiki/Brad_Bird" > Brad Bird </td>
+      <td>Action</td>
+      <td>5,55$</td>
+    </tr>
+    <tr>
+      <td>8</td>
+      <td><a href="https://www.youtube.com/watch?v=wX8fhP-fsPg" ><img src="mi5.jpg" width="100%" height="100%" ></a></td>
+      <td>L'équipe IMF est dissoute et Ethan Hunt se retrouve désormais isolé, alors que le groupe doit affronter un réseau d'agents spéciaux particulièrement entraînés, le Syndicat.</td>
+      <td><a href="https://fr.wikipedia.org/wiki/Christopher_McQuarrie" > Christopher McQuarrie</td>
+      <td>Action</td>
+      <td>5,55$</td>
+    </tr>
+    <tr>
+      <td>9</td>
+      <td><a href="https://www.youtube.com/watch?v=8y5oSx3pB-s" ><img src="mi6.jpg" width="100%" height="100%" ></a></td>
+      <td>Ethan Hunt et l'équipe du FMI se joignent à August Walker, l'assassin de la CIA, pour éviter une catastrophe d'une ampleur extrême. Le marchand d'armes John Lark et un groupe de terroristes connus prévoient utiliser trois noyaux de plutonium pour une attaque nucléaire simultanée sur le Vatican, Jérusalem et La Mecque.</td>
+      <td><a href="https://fr.wikipedia.org/wiki/Christopher_McQuarrie" > Christopher McQuarrie</td>
+      <td>Action</td>
+      <td>5,55$</td>
+    </tr>
+    <tr>
+      <td>10</td>
+      <td><a href="https://www.youtube.com/watch?v=vIpqpRuGrq4" ><img src="mi2.jpg" width="100%" height="100%" ></a></td>
+      <td>L'agent secret Ethan Hunt, du MIF, recrute deux assistants, Luther Strickell et Billy Baird, ainsi qu'une cambrioleuse de charme, Nyah Hall, en vue d'une mission de la plus haute importance.</td>
+      <td><a href="https://fr.wikipedia.org/wiki/John_Woo" >John Woo </td>
+      <td>Action</td>
+      <td>5,55$</td>
+    </tr>
+    <tr>
+      <td>11</td>
+      <td>Doe</td>
+      <td>Doe</td>
+      <td></td>
+      <td>Doe</td>
+      <td>5,55$</td>
+    </tr>
+    <tr>
+      <td>12</td>
+      <td>Doe</td>
+      <td>Doe</td>
+      <td></td>
+      <td>Doe</td>
+      <td>5,55$</td>
+    </tr>
+    <tr>
+      <td>13</td>
+      <td>Doe</td>
+      <td>Doe</td>
+      <td></td>
+      <td>Doe</td>
+      <td>5,55$</td>
+    </tr>
+    <tr>
+      <td>14</td>
+      <td>Doe</td>
+      <td>Doe</td>
+      <td></td>
+      <td>Doe</td>
+      <td>5,55$</td>
+    </tr>
+    <tr>
+      <td>15</td>
+      <td>Doe</td>
+      <td>Doe</td>
+      <td></td>
+      <td>Doe</td>
+      <td>5,55$</td>
+    </tr>
+    <tr>
+      <td>16</td>
+      <td>Doe</td>
+      <td>Doe</td>
+      <td></td>
+      <td>Doe</td>
+      <td>5,55$</td>
+    </tr>
+    <tr>
+      <td>17</td>
+      <td>Doe</td>
+      <td>Doe</td>
+      <td></td>
+      <td>Doe</td>
+      <td>5,55$</td>
+    </tr>
+    <tr>
+      <td>18</td>
+      <td>Doe</td>
+      <td>Doe</td>
+      <td></td>
+      <td>Doe</td>
+      <td>5,55$</td>
+    </tr>
+    <tr>
+      <td>19</td>
+      <td>Doe</td>
+      <td>Doe</td>
+      <td></td>
+      <td>Doe</td>
+      <td>5,55$</td>
+    </tr>
+    <tr>
+      <td>20</td>
+      <td>Doe</td>
+      <td>Doe</td>
+      <td></td>
+      <td>Doe</td>
+      <td>5,55$</td>
+    </tr>
+    
+    </p>
+  <p>
+    <form class="boutton"> 
+      <button type="submit" id="boutton"><a href="Accueil.html" >Accueil</button>
         <br>
-      <button type="submit" id="bouton2"><a href="panier.php">panier</button>
+      <button type="submit" id="boutton"><a href="panier.php">panier</button>
         <br>
-      <button type="submit" id="bouton3"><a href="souscription.php">souscription</button>
+      <button type="submit" id="boutton"><a href="souscription.php">souscription</button>
         <br>
-      <button type="submit" id="bouton4"><a href="desouscription.php">desouscription</button>
+      <button type="submit" id="boutton"><a href="desouscription.php">desouscription</button>
     </form>
+    </p>
   </body>
 </html>
