a1acd238382a446dc9779d964ce9401e4d6668bc
JuloushVolley <julien.schoeffre@sfr.fr>
2023-03-14 15:44:49 +0100

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -38,30 +38,35 @@
     <p>
       <form action="connexion.php" method="POST">
             <input name="pseudo">
-            <input name="mpd">
+            <input name="mdp">
             <button type="submit" name="send">connexion</button>
       </form>
     </p>
     <?php
             session_start();
-            @$pseudo=$_POST["pseudo"];
-            @$mpd=$_POST["mdp"];
-            @$valider=$_POST["sen"];
+            $pseudo=$_POST["pseudo"];
+            $mdp=$_POST["mdp"];
             $erreur="";
-            if(isset($valider)){
-                include("connexion2.php");
-                $sel=$pdo->prepare('SELECT * from users where pseudo=? and mpd=? limit 1');
-                $sel->execute(array($pseudo,$mpd));
-                $tab=$sel->fetchAll();
-                if(count($tab)>0){
-                    $_SESSION["pseudo"]=ucfirst(strtolower($tab[0]["pseudo"])).
-                    " ".strtoupper($tab[0]["pseudo"]);
-                    $_SESSION["pseudo"]="oui";
-                    header('location:panier.php');
-                    echo ("bien jouer");
-                }
-                else
-                    $erreur="Mauvais login ou mot de passe!";
+            if (isset($_POST['send']))
+            {
+               if (empty($pseudo)) $erreur = "Veuillez renter votre pseudo";
+               elseif(empty($mdp)) $erreur = "veuillez renter votre mot de passe";
+
+               include("connexion2.php");
+               $sql= $db->prepare("SELECT * FROM users WHERE pseudo = ? limit 1");
+               $sql->execute([$_POST["pseudo"]]);
+               $hash = $sql->fetch();
+
+               if ($hash && password_verify($_POST["mdp"],$hash["mdp"]))
+               {
+                  $_SESSION["pseudo"]=$_POST["pseudo"];
+
+                  header("location:panier.php");
+                  echo "bonjour";
+               }
+               else{
+                  $erreur="Mauvais pseudo ou mot de passe";
+               }
             }
             ?>
     </body>
