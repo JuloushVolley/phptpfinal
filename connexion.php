787c20b41e8c584bfda816eae12be520f103b076
JuloushVolley <julien.schoeffre@sfr.fr>
2023-03-15 14:05:21 +0100

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -1,73 +1,67 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Page d'accueil</title>
+    <title>connexion</title>
     <meta charset="utf-8" />
-    
-    <style>
-         *{
-            font-family:arial;
-         }
-         body{
-            margin:20px;
-         }
-         input{
-            border:solid 1px #2222AA;
-            margin-bottom:10px;
-            padding:16px;
-            outline:none;
-            border-radius:6px;
-         }
-         .erreur{
-            color:#CC0000;
-            margin-bottom:10px;
-         }
-         a{
-            font-size:12pt;
-            color:#EE6600;
-            text-decoration:none;
-            font-weight:normal;
-         }
-         a:hover{
-            text-decoration:underline;
-         }
-      </style>
    </head>
   </head>
   <body>
     <p>
       <form action="connexion.php" method="POST">
-            <input name="pseudo">
-            <input name="mdp">
+            <input name="pseudo" required>
+            <input name="mdp" required>
             <button type="submit" name="send">connexion</button>
       </form>
     </p>
     <?php
-            session_start();
-            $pseudo=$_POST["pseudo"];
-            $mdp=$_POST["mdp"];
-            $erreur="";
-            if (isset($_POST['send']))
-            {
-               if (empty($pseudo)) $erreur = "Veuillez renter votre pseudo";
-               elseif(empty($mdp)) $erreur = "veuillez renter votre mot de passe";
-
-               include("connexion2.php");
-               $sql= $db->prepare("SELECT * FROM users WHERE pseudo = ? limit 1");
-               $sql->execute([$_POST["pseudo"]]);
-               $hash = $sql->fetch();
-
-               if ($hash && password_verify($_POST["mdp"],$hash["mdp"]))
+            if (isset($_POST["pseudo"]) && isset($_POST["mdp"])){
+               session_start();
+               $pseudo=$_POST["pseudo"];
+               $mdp=$_POST["mdp"];
+               $erreur="";
+               if (isset($_POST['send']))
                {
-                  $_SESSION["pseudo"]=$_POST["pseudo"];
+                  if (empty($pseudo)) {
+                     $erreur = "Veuillez renter votre pseudo";
+                     echo $erreur;
+                  }
+                  elseif(empty($mdp)) {
+                     $erreur = "veuillez renter votre mot de passe";
+                     echo $erreur;
+                  }
 
-                  header("location:panier.php");
-                  echo "bonjour";
-               }
-               else{
-                  $erreur="Mauvais pseudo ou mot de passe";
-               }
+                  $db=new PDO("mysql:host=localhost;dbname=tpfinal","root","");
+                  $sql= $db->prepare("SELECT pseudo,mdp FROM users WHERE pseudo = ?");
+                  $sql->execute([$pseudo]);
+                  $hash = $sql->fetch();
+                  echo $hash['mdp'];
+                 
+                  
+                  if ($hash != 0){
+                     echo "bonjour";
+                     echo $mdp;
+                     echo var_dump(password_verify($mdp,$hash['mdp']));
+                     if (password_verify($mdp,$hash['mdp']))
+                     {
+                        $_SESSION["pseudo"]=$pseudo;
+                        $_SESSION["mdp"]=$mdp;
+                        echo $_SESSION["pseudo"];
+                        echo $_SESSION["mdp"];
+                        echo "conecté";
+                        header("location:panier.php");
+                     
+                     }
+                     // else{
+                     //    $erreur="Mauvais pseudo ou mot de passe";
+                     //    echo $erreur;
+                     // }
+                  }
+
+
+               
+             
             }
+         }
             ?>
     </body>
 </html>
