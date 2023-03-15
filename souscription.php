f79891fbffb943e666c6e8a2361dc6ec1c6c6d13
JuloushVolley <julien.schoeffre@sfr.fr>
2023-03-15 17:00:15 +0100

    Update souscription.php

--- souscription.php
+++ souscription.php
@@ -1,17 +1,62 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Page d'accueil</title>
+    <title>inscription</title>
     <meta charset="utf-8" />
     <link rel="stylesheet" type="text/css" href="tpfinal.css" />
+    <style type="text/css">
+      #form1 {
+        max-width: 500px;
+        margin: 0 auto;
+      }
+      input[name="pseudo"],
+      [name="mail"],
+      [name="mdp"],
+      [name="telephone"] {
+        display: block;
+        width: 100%;
+        padding: 10px;
+        margin-bottom: 20px;
+        border: none;
+        border-radius: 4px;
+        background-color: #f3f3f3;
+      }
+      #b1 {
+        display: block;
+        width: 100%;
+        padding: 10px;
+        border: none;
+        border-radius: 4px;
+        background-color: #4caf50;
+        color: white;
+        cursor: pointer;
+        font-size: 16px;
+        font-weight: bold;
+      }
+      #b2 {
+        width: 20%;
+        padding: 10px;
+        border: none;
+        border-radius: 4px;
+        background-color: red;
+        color: white;
+        cursor: pointer;
+        font-size: 16px;
+        font-weight: bold;
+        margin-right: 10px;
+      }
+      .button-group {
+        display: flex; 
+      }
+      </style>
   </head>
   <body>
-      <form action="souscription.php" method="POST">
-                <input name="pseudo" value="pseudo">
-                <input name="mail">
-                <input name="mdp">
-                <input name="telephone">
-                <button type="submit" name="send">inscription</button>
+      <form action="souscription.php" method="POST" id="form1">
+                <input name="pseudo" value="pseudo" id="f1">
+                <input name="mail"   value="mail" id="f1">
+                <input name="mdp" value="mdp" id="f1">
+                <input name="telephone" value="telephone" id="f1">
+                <button type="submit" name="send" id="b1">inscription</button>
         </form>
         <?php
              $db = new PDO("mysql:host=localhost;dbname=tpfinal",'root','');
@@ -60,14 +105,11 @@
         <p id="p1">
           Bonjour veuilleiez renseigner vous coorodonnées.
         </p>
-        <form>
-          <button type="submit" id="bouton1"><a href="film.php" >film</button>
-            <br>
-          <button type="submit" id="bouton2"><a href="panier.php">panier</button>
-            <br>
-          <button type="submit" id="bouton3"><a href="Accueil.html">accueil</button>
-            <br>
-          <button type="submit" id="bouton4"><a href="desouscription.php">désinscription</button>
-    </form>
+        <div class="button.group">
+          <button type="submit" id="b2"><a href="film.php" >film</button>
+          <button type="submit" id="b2"><a href="panier.php">panier</button>
+          <button type="submit" id="b2"><a href="Accueil.html">accueil</button>
+          <button type="submit" id="b2"><a href="desouscription.php">désinscription</button>
+        </div>
   </body>
 </html>
